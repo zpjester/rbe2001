0d935a67460eb65c60b578bfa43f6412fd87e518
zpjester <58483011+zpjester@users.noreply.github.com>
2021-01-29 16:22:29 -0500

    Removed local system references from cpp properties

--- RBE_2001_Base_Code/src/main.cpp
+++ RBE_2001_Base_Code/src/main.cpp
@@ -3,8 +3,10 @@
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(13, HIGH);
 }
