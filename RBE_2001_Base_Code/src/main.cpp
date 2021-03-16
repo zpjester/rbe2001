b424344547836a959d8ac7fddaa5f1b67e818162
zpjester <58483011+zpjester@users.noreply.github.com>
2021-03-16 18:46:44 -0400

    Arm Control

--- RBE_2001_Base_Code/src/main.cpp
+++ RBE_2001_Base_Code/src/main.cpp
@@ -1,9 +1,16 @@
 //
 #include <Romi.cpp>
+<<<<<<< Updated upstream
 #include <fourBar.h>
 #include <servo32u4.h>
 Romi robot = Romi();
 fourBar lift;
+=======
+#include <blueMotor.h>
+#include <servo32u4.h>
+Romi robot = Romi();
+blueMotor armMotor = blueMotor();
+>>>>>>> Stashed changes
 Servo32U4 gripper = Servo32U4();
 // IRDecoder decoder(14);
 enum driveType{initialize, driving, turning};
@@ -17,6 +24,8 @@ gripper.Init();
 lift.setup();
 lift.reset();
 driveState = initialize;
+gripper.Attach();
+gripper.Init();
 }
 void loop(){
 //Put your loop code here, to run repeatedly.
